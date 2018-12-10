3187a2e91a41956a0146a7a701c91e380cd22eff
ojarce123 <45667231+ojarce123@users.noreply.github.com>
2018-12-10 15:52:08 -0700

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,22 @@
+// set count
+if (localStorage.getItem("count") == null){
+	localStorage.setItem("count",0)
+}
+//update count
+function update(){
+	document.getElementById("count").innerHTML = localStorage.getItem("count")
+}
+update()
+//add one to count
+function plusOne(){
+	localStorage.setItem("count", Number(localStorage.getItem("count")) + 1)
+	update()
+}
+
+//reset count
+function reset(){
+	if (confirm("Are you sure I didn't tell you so")){
+		localStorage.setItem("count", 0)
+		update()
+	}
+}
